af6f06046f7c48dd612192b3bb0d746638f6fee0
atreeon <atreeonhill@gmail.com>
2023-07-05 10:49:48 +0100

    SEO Update

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
 module.exports = {
-	"title": "Clean Air for Brighton & Hove - cleanairbh.com",
+	"title": "Clean Air for Brighton & Hove - cleanairbh.com - Air Pollution - Air Quality",
 	"url": "https://CleanAirBH.com/",
 	"language": "en",
-	"description": "This is the website for the group Clean Air for Brighton & Hove.  " +
-		"We campaign for better air quality in the city. " +
-		"We meet, share resources and discuss Brighton & Hove's air quality.",
+	"description": "Clean Air for Brighton & Hove." +
+		"We campaign for better air quality in the city." +
+		"We want to reduce the high air pollution levels in Brighton & Hove.",
 	"email": "cleanairbh@gmail.com",
 	"twitterUrl": "https://www.twitter.com/cleanairbh",
 }
